7d9ecb8b327ea2f21ddbc44e7087191ad9cbac45
Jakub Glinka <glinka.jakub@outlook.com>
2019-12-08 03:34:20 +0100

    init

---
+++ src/main/java/com/glinka/mtab/MtabApplication.java
@@ -0,0 +1,13 @@
+package com.glinka.mtab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtabApplication.class, args);
+    }
+
+}
